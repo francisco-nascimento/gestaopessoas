125e95d1ae9fe6349e50ba987853e9aaf8413ef1
Francisco do Nascimento Junior <chico.nascimento@gmail.com>
2019-12-20 10:27:24 -0300

    Versao inicial UC001-Inserir Funcionario

---
+++ src/main/java/br/ifpe/web2/controller/FuncionarioController.java
@@ -0,0 +1,54 @@
+package br.ifpe.web2.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.ifpe.web2.model.cadastro.Funcionario;
+import br.ifpe.web2.service.CargoService;
+import br.ifpe.web2.service.EmpresaService;
+import br.ifpe.web2.service.FuncionarioService;
+import br.ifpe.web2.util.ServiceException;
+
+@Controller
+public class FuncionarioController {
+
+	@Autowired
+	private FuncionarioService funcService;
+	
+	@Autowired
+	private CargoService cargoService;
+
+	@Autowired
+	private EmpresaService empresaService;
+
+	@GetMapping("/formFunc")
+	public String exibirFormFunc(Funcionario funcionario, Model model) {
+		model.addAttribute("listaCargos", this.cargoService.listarTodos(true));
+		model.addAttribute("listaEmpresas", this.empresaService.listarTodos(true));
+		return "/funcionario/funcionario-form";
+	}
+	
+	@PostMapping("/inserirFuncionario")
+	public String inserirFuncionario(@Valid Funcionario funcionario, BindingResult br, Model model, RedirectAttributes ra) {
+		if (br.hasErrors()) {
+			return exibirFormFunc(funcionario, model);
+		}
+		try {
+			this.funcService.inserirFuncionario(funcionario);
+			ra.addFlashAttribute("mensagem", "Funcionário Cadastrado com Sucesso");
+			return "redirect:/formFunc";
+		} catch (ServiceException e) {
+			br.addError(new ObjectError("global", e.getMessage()));
+			return exibirFormFunc(funcionario, model); 
+		} 
+		
+	}
+}
---
+++ src/main/java/br/ifpe/web2/service/FuncionarioService.java
@@ -0,0 +1,30 @@
+package br.ifpe.web2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ifpe.web2.dao.FuncionarioDAO;
+import br.ifpe.web2.model.cadastro.Funcionario;
+import br.ifpe.web2.util.ServiceException;
+import br.ifpe.web2.util.Util;
+
+@Service
+public class FuncionarioService {
+
+	@Autowired
+	private FuncionarioDAO funcionarioDAO;
+	
+	public void inserirFuncionario(Funcionario funcionario) throws ServiceException {
+		
+		Funcionario funcExistente = this.funcionarioDAO.findByCpf(funcionario.getCpf());
+		
+		if (funcExistente != null) {
+			throw new ServiceException("Já existe um funcionário cadastrado com este CPF");
+		}
+		if (Util.calcularIdade(funcionario.getDataNascimento()) < 18) {
+			throw new ServiceException("Funcionário deve ter 18 anos ou mais");
+		}
+		
+		this.funcionarioDAO.save(funcionario);
+	}
+}
